19e61e2086e09c0f8b10b2e6a48991bf0667b729
Arya Chandra Kusuma <aryachandrak14@gmail.com>
2024-03-14 10:23:58 +0700

    Jobsheet 4 - praktikum 2.2 Not Found Exceptions

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -9,10 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class userController extends Controller
 {
     public function index(){
-        $user = userModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
-
+        $user = userModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data'=>$user]);
     }
 }
