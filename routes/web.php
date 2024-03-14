9305d1bf19f45f257896f381235406d2cd07df32
Arya Chandra Kusuma <aryachandrak14@gmail.com>
2024-03-14 15:21:19 +0700

    Jobsheet 4 - Praktikum 2.5 Atribute Change dan 2.6 CRUD

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@ Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 
 Route::get('/user', [userController::class, 'index']);
+
+Route::get('/user/tambah', [userController::class, 'tambah']);
+
+Route::post('/user/tambah_simpan', [userController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [userController::class, 'ubah']);
+
+Route::put('/user/ubah_simpan/{id}', [userController::class, 'ubah_simpan']);
+
+Route::get('/user/hapus/{id}', [userController::class, 'hapus']);
